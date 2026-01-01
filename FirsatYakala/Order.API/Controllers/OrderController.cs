82d3a7a411763500fb7c20179be0b5ecdb5b2574
ozzkan-burak <brk.ozzkan@gmail.com>
2026-01-01 23:25:12 +0300

    episode9 _ Feat: StockController and OrderController imrovement

--- FirsatYakala/Order.API/Controllers/OrderController.cs
+++ FirsatYakala/Order.API/Controllers/OrderController.cs
@@ -9,6 +9,8 @@ namespace Order.API.Controllers
   public class OrderController : ControllerBase
   {
     private readonly OrderDbContext _context;
+    private readonly HttpClient _httpClient;
+
     public OrderController(OrderDbContext context)
     {
       _context = context;
@@ -16,9 +18,34 @@ namespace Order.API.Controllers
     [HttpPost]
     public async Task<IActionResult> CreateOrder(Models.Order order)
     {
+      var productId = order.Items.First().ProductId;
+
+      var response = await _httpClient.GetAsync($"http://localhost:5002/api/stocks/{productId}");
+      
+      if(!response.IsSuccessStatusCode)
+      {
+        return BadRequest("Stock not found");
+      }
+      var stockQuantityString = await response.Content.ReadAsStringAsync();
+      var stockQuantity = int.Parse(stockQuantityString);
+
+      if(stockQuantity < 1)
+      {
+        return BadRequest("Stock quantity is not enough");
+      }
+      order.Status = Enums.OrderStatus.Completed;
       await _context.Orders.AddAsync(order);
       await _context.SaveChangesAsync();
-      return Ok(new {OrderId = order.Id});
-    }
+      var decreaseRequest = new 
+      { 
+        ProductId = productId, 
+        Count = 1 
+    };
+      var decreaseResponse = await _httpClient.PostAsJsonAsync($"http://localhost:5002/api/stocks/decrease-stock", decreaseRequest);
+      if(!decreaseResponse.IsSuccessStatusCode)
+      {
+        return BadRequest("Stock not found");
+      }
+return Ok(new { OrderId = order.Id, Message = "Sipariş alındı ve stok düşüldü!" });    }
   }
 }
--- FirsatYakala/Stock.API/Controllers/StockController.cs
+++ FirsatYakala/Stock.API/Controllers/StockController.cs
@@ -29,5 +29,23 @@ namespace Stock.API.Controllers
             await _context.SaveChangesAsync();
             return Ok(stock);
         }
+        // Stok düşür
+        [HttpPost("decrease-stock")]
+        public async Task<IActionResult> DecreaseStock([FromBody] StockUpdateDto model)
+        {
+           var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.ProductId == model.ProductId);
+           if(stock == null) return NotFound("Stock not found");
+           if(stock.Count < model.Count) return BadRequest("Not enough stock");
+           stock.Count -= model.Count;
+           await _context.SaveChangesAsync();
+           return Ok(new {Message = "Stock updated successfully", NewCount = stock.Count});
+        }
+        
     }
+}
+
+public class StockUpdateDto
+{
+    public string ProductId { get; set; }
+    public int Count { get; set; }
 }
